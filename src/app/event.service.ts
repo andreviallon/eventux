818cd35cca0011c7614344cc951b9189f0761b53
andre_viallon <viallon.andre@gmail.com>
2020-06-03 19:07:40 +0200

    Update card event UI

--- src/app/event.service.ts
+++ src/app/event.service.ts
@@ -9,16 +9,19 @@ export interface IEvent {
   endTime: string;
   price: number;
   tags: string[];
+  img: string;
   location: {
     address: string;
     city: string;
     zipcode: string;
     country: string;
+    phoneNumber: number;
   }
   tutor: {
     _tutorId: string;
     firstName: string;
     lastName: string;
+    description: string;
     img: string;
   }
 }
@@ -33,22 +36,25 @@ export class EventService {
     {
       _id: '1',
       title: 'Event 1',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      description: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?',
       date: '12 Dec 2020',
       startTime: '10.00',
       endTime: '12.00',
-      price: 120.00,
+      price: 120,
+      img: './assets/event-image.png',
       tags: ['React', 'JavaScript', 'Frontend Dev'],
       location: {
-        address: 'Jernbanegade, 201',
-        city: 'Kolding',
-        zipcode: '6000',
-        country: 'Denmark'
+        address: 'H. C. Andersens Blvd. 11, 1. th',
+        city: 'Copenhagen',
+        zipcode: '1553',
+        country: 'Denmark',
+        phoneNumber: 30303030
       },
       tutor: {
         _tutorId: '123',
         firstName: 'John',
         lastName: 'Doe',
+        description: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores.',
         img: './src/app/assets/john-doe-avatar.png'
       }
     },
@@ -59,18 +65,21 @@ export class EventService {
       date: '13 Dec 2020',
       startTime: '11.00',
       endTime: '15.00',
-      price: 100.00,
+      price: 100,
       tags: ['React', 'JavaScript', 'Frontend Dev'],
+      img: './assets/event-image.png',
       location: {
-        address: 'Jernbanegade, 201',
-        city: 'Kolding',
-        zipcode: '6000',
-        country: 'Denmark'
+        address: 'H. C. Andersens Blvd. 11, 1. th',
+        city: 'Copenhagen',
+        zipcode: '1553',
+        country: 'Denmark',
+        phoneNumber: 30303030
       },
       tutor: {
         _tutorId: '123',
         firstName: 'John',
         lastName: 'Doe',
+        description: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores.',
         img: ''
       }
     },
@@ -81,18 +90,21 @@ export class EventService {
       date: '15 Dec 2020',
       startTime: '09.00',
       endTime: '12.00',
-      price: 100.00,
+      price: 100,
       tags: ['React', 'JavaScript', 'Frontend Dev'],
+      img: './assets/event-image.png',
       location: {
-        address: 'Jernbanegade, 201',
-        city: 'Kolding',
-        zipcode: '6000',
-        country: 'Denmark'
+        address: 'H. C. Andersens Blvd. 11, 1. th',
+        city: 'Copenhagen',
+        zipcode: '1553',
+        country: 'Denmark',
+        phoneNumber: 30303030
       },
       tutor: {
         _tutorId: '456',
         firstName: 'Andre',
         lastName: 'Viallon',
+        description: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores.',
         img: ''
       }
     },
@@ -103,17 +115,20 @@ export class EventService {
       date: '15 Dec 2020',
       startTime: '09.00',
       endTime: '12.00',
-      price: 90.00,
+      price: 90,
       tags: ['React', 'JavaScript', 'Frontend Dev'],
+      img: './assets/event-image.png',
       location: {
-        address: 'Jernbanegade, 201',
-        city: 'Kolding',
-        zipcode: '6000',
-        country: 'Denmark'
+        address: 'H. C. Andersens Blvd. 11, 1. th',
+        city: 'Copenhagen',
+        zipcode: '1553',
+        country: 'Denmark',
+        phoneNumber: 30303030
       },
       tutor: {
         _tutorId: '456',
         firstName: 'Andre',
+        description: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores.',
         lastName: 'Viallon',
         img: ''
       }
@@ -125,18 +140,21 @@ export class EventService {
       date: '15 Dec 2020',
       startTime: '09.00',
       endTime: '12.00',
-      price: 90.00,
+      price: 90,
       tags: ['React', 'JavaScript', 'Frontend Dev'],
+      img: './assets/event-image.png',
       location: {
-        address: 'Jernbanegade, 201',
-        city: 'Kolding',
-        zipcode: '6000',
-        country: 'Denmark'
+        address: 'H. C. Andersens Blvd. 11, 1. th',
+        city: 'Copenhagen',
+        zipcode: '1553',
+        country: 'Denmark',
+        phoneNumber: 30303030
       },
       tutor: {
         _tutorId: '456',
         firstName: 'Andre',
         lastName: 'Viallon',
+        description: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores.',
         img: ''
       }
     },
@@ -147,18 +165,21 @@ export class EventService {
       date: '15 Dec 2020',
       startTime: '09.00',
       endTime: '12.00',
-      price: 90.00,
+      price: 90,
       tags: ['React', 'JavaScript', 'Frontend Dev'],
+      img: './assets/event-image.png',
       location: {
-        address: 'Jernbanegade, 201',
-        city: 'Kolding',
-        zipcode: '6000',
-        country: 'Denmark'
+        address: 'H. C. Andersens Blvd. 11, 1. th',
+        city: 'Copenhagen',
+        zipcode: '1553',
+        country: 'Denmark',
+        phoneNumber: 30303030
       },
       tutor: {
         _tutorId: '456',
         firstName: 'Andre',
         lastName: 'Viallon',
+        description: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores.',
         img: ''
       }
     }
