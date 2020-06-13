8cbe2225863a2e0a733e79c0b785bfcf8fe88c63
andre_viallon <viallon.andre@gmail.com>
2020-06-13 13:36:35 +0200

    WIP: setup node

--- server.js
+++ server.js
@@ -11,23 +11,25 @@ connectDB();
 const events = require('./routes/events');
 const app = express();
 
-app.use(express.json());
+app.get('/', (req, res) => res.send('Hello'));
 
-if (process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'));
-}
+// app.use(express.json());
 
-app.use('/api/v1/events', events);
+// if (process.env.NODE_ENV === 'development') {
+//     app.use(morgan('dev'));
+// }
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
-}
+// app.use('/api/v1/events', events);
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
-}
+// if (process.env.NODE_ENV === 'development') {
+//     app.use(express.static('client/build'));
+//     app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+// }
+
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static('client/build'));
+//     app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+// }
 
 const PORT = process.env.PORT || 5000;
 
