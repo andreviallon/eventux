f6adaf2bb508a38227b8a2979e9b2881745d712c
andre_viallon <viallon.andre@gmail.com>
2020-06-18 17:46:50 +0200

    Move event detail logic to parent comp

--- client/src/app/components/event-detail/event-detail.component.ts
+++ client/src/app/components/event-detail/event-detail.component.ts
@@ -1,39 +1,24 @@
+import { IEvent } from './../../state/event.model';
 import { EventService } from '../../event.service';
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
 import { faLongArrowAltRight, faShare } from '@fortawesome/free-solid-svg-icons';
 import { faTwitter, faFacebookF } from '@fortawesome/free-brands-svg-icons';
-import { IEvent } from '../../state/event.model';
 
 @Component({
   selector: 'app-event-detail',
   templateUrl: './event-detail.component.html',
   styleUrls: ['./event-detail.component.scss']
 })
-export class EventDetailComponent implements OnInit, OnDestroy {
+export class EventDetailComponent {
 
-  public event: IEvent;
+  @Input() event: IEvent;
+  @Input() relatedEvents: IEvent[]
+
   public faLongArrowAltRight = faLongArrowAltRight;
   public faShare = faShare;
   public faTwitter = faTwitter;
   public faFacebook = faFacebookF;
-  public relatedEvents: IEvent[];
 
-
-  private subscription = new Subscription();
-
-  constructor(private route: ActivatedRoute, private eventService: EventService) { }
-
-  ngOnInit() {
-    this.subscription.add(
-      this.route.params.subscribe(params => this.event = this.eventService.getEvent(params['id']))
-    );
-
-    this.relatedEvents = this.eventService.getRelatedEvents();
-  }
-
-  ngOnDestroy(): void {
-    this.subscription.unsubscribe();
-  }
 }
--- client/src/app/connected-components/event-detail-page/event-detail-page.component.ts
+++ client/src/app/connected-components/event-detail-page/event-detail-page.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { IEvent } from './../../state/event.model';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { EventService } from 'src/app/event.service';
 
 @Component({
   selector: 'app-event-detail-page',
   templateUrl: './event-detail-page.component.html',
   styleUrls: ['./event-detail-page.component.scss']
 })
-export class EventDetailPageComponent {
+export class EventDetailPageComponent implements OnInit, OnDestroy {
+
+  public event: IEvent;
+  public relatedEvents: IEvent[];
+
+  private subscription = new Subscription();
+
+  constructor(private route: ActivatedRoute, private eventService: EventService) { }
+
+  ngOnInit() {
+    this.subscription.add(
+      this.route.params.subscribe(params => this.event = this.eventService.getEvent(params['id']))
+    );
+
+    this.relatedEvents = this.eventService.getRelatedEvents();
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 
 }
