5570910e93bb30c1bea2c1082d8bdea7651c8f3a
andre_viallon <viallon.andre@gmail.com>
2020-08-08 09:32:15 +0200

    continue working on order summary page

--- client/src/app/order-summary/order-summary.component.ts
+++ client/src/app/order-summary/order-summary.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { IEventIncTeacherAndVenue } from '../state/event/event.model';
+import { convertDate } from '../utils/convert-date';
 
 @Component({
   selector: 'app-order-summary',
   templateUrl: './order-summary.component.html',
   styleUrls: ['./order-summary.component.scss']
 })
-export class OrderSummaryComponent implements OnInit {
+export class OrderSummaryComponent {
 
-  constructor() { }
+  @Input() event: IEventIncTeacherAndVenue;
 
-  ngOnInit(): void {
+  public getCourseDate() {
+    return convertDate(this.event.courseDate);
   }
 
 }
