eb23a25f394201e2fe3d1ea8ee27a247894697ca
andre_viallon <viallon.andre@gmail.com>
2020-08-16 12:16:38 +0200

    add teacher schema

---
+++ models/Teacher.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const TeacherSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        trim: true,
+        required: [true, 'Please add a first name']
+    },
+    lastName: {
+        type: String,
+        trim: true,
+        required: [true, 'Please add a last name']
+    },
+    description: {
+        type: String,
+        trim: true
+    },
+    img: {
+        type: String,
+        trim: true,
+        required: [true, 'Please add an image']
+    },
+    email: {
+        type: String,
+        trim: true,
+        required: [true, 'Please add an email']
+    },
+    phoneNumber: {
+        type: Number,
+        trim: true,
+        required: [true, 'Please add a phone number']
+    },
+    
+});
+
+module.exports = mongoose.model('Teacher', TeacherSchema);
